2d28b725177db0c4dd19346fb040c7d5f14fa66b
Isik Erhan <isik.erhan.07@gmail.com>
2024-11-03 00:53:47 +0300

    feat(auto-revert): implement auto-skip and squash in auto-revert

--- lib/git/core.bash
+++ lib/git/core.bash
@@ -27,6 +27,19 @@ get_current_git_author() {
   echo "$(git config user.name) <$(git config user.email)>"
 }
 
+# get root directory of the current git repository
+# usage get_repository_root_dir
+get_repository_root_dir() {
+  git rev-parse --show-toplevel
+}
+
+# get git directory of the current git repository
+# usage get_git_dir
+get_git_dir() {
+  local rootdir=$(get_repository_root_dir)
+  echo "$rootdir/.git"
+}
+
 # check if current git repository has staged changes. returns 0 if so, non-zero otherwise
 # usage: has_staged_changes
 has_staged_changes() {
@@ -40,4 +53,22 @@ has_unstaged_changes() {
   git diff --exit-code >/dev/null 2>&1
   local ret=$?
   ! [ $ret -eq 0 ]
+}
+
+# check if current git repository has a sequencer. this indicates that there's an ongoing revert or cherry-pick
+# usage: has_sequencer
+has_sequencer() {
+  local gitdir="$(get_git_dir)"
+  [ -d "$gitdir/sequencer" ]
+}
+
+# get next todo item from the sequencer
+# usage: get_next_todo
+get_next_todo() {
+  if ! has_sequencer; then
+    return
+  fi
+
+  local gitdir="$(get_git_dir)"
+  echo "$(head -n 1 "$gitdir/sequencer/todo")"
 }
