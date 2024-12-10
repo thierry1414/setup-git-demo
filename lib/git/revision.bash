afb07f3a28a4fe59c71530c592e0c4ef9bc489de
Isik Erhan <isik.erhan.07@gmail.com>
2024-12-10 21:57:55 +0300

    refactor(core): replace direct usages of positional arguments with local variables in lib/git/revision.bash

--- lib/git/revision.bash
+++ lib/git/revision.bash
@@ -6,13 +6,17 @@ readonly __LIB_GIT_REVISION__=true
 # echoes the commit hash of the speficied revision
 # usage: resolve_revision <revision>
 resolve_revision() {
-  echo "$(git show -s --pretty="%H" "$1" 2>/dev/null)"
+  local revision="$1"
+
+  echo "$(git show -s --pretty="%H" "$revision" 2>/dev/null)"
 }
 
 # checks if revision exists, returns 0 if so, otherwise non-zero
 # usage: revision_exists <revision>
 revision_exists() {
-  git show -s --pretty="%H" "$1" >/dev/null 2>&1
+  local revision="$1"
+
+  git show -s --pretty="%H" "$revision" >/dev/null 2>&1
 }
 
 # returns number of commits between two revisions in current tree
@@ -21,7 +25,7 @@ num_commits_between() {
   local from="$1"
   local to="$2"
 
-  git rev-list --count $2 $1 2>/dev/null
+  git rev-list --count "$to" "$from" 2>/dev/null
 }
 
 # returns commit hashes between two revisions in current tree
@@ -30,5 +34,5 @@ commits_between() {
   local from="$1"
   local to="$2"
 
-  git rev-list $2 $1 2>/dev/null
+  git rev-list "$to" "$from" 2>/dev/null
 }
