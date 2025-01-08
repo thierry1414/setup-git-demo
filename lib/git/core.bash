c1a722074665d74005b2726413d25ff155e716f8
Isik Erhan <isik.erhan.07@gmail.com>
2025-01-08 23:44:14 +0300

    docs: attach license notice to the source files

--- lib/git/core.bash
+++ lib/git/core.bash
@@ -1,3 +1,22 @@
+# Git Tools. A set of custom Git commands that might come in handy for certain tasks.
+#
+# Copyright (C) 2025 Isik Erhan
+#
+# This file is part of Git Tools.
+#
+# Git Tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Git Tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Git Tools.  If not, see <https://www.gnu.org/licenses/>.
+
 if [ -n "$__LIB_GIT_CORE__" ]; then
   return
 fi
